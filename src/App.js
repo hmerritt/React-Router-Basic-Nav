3b189b19acea4c3407b40ec60c2147d2066b30a4
Harry Merritt <harry@merritt.es>
2020-01-16 00:26:44 +0000

    Completed steps laid out in readme.me

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
-  <div>
-    <Navigation />
-  </div>
+    <div>
+        <Navigation />
+
+        {/* Routes */}
+        <Route path="/" exact  />
+        <Route path="/about"   />
+        <Route path="/contact" />
+    </div>
 );
 
 export default App;
